00bcb94fb53a841b154ddbd36c09f018d2ccc00f
umeshstar <umeshahire57@gmail.com>
2023-06-12 00:21:33 +0530

    changes by umesh for method argumentnotexception handle

--- src/main/java/com/bikkadit/electronicstroe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bikkadit/electronicstroe/exception/GlobalExceptionHandler.java
@@ -5,13 +5,21 @@ import com.bikkadit.electronicstroe.helper.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    //handling to resource not found exception
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex)
     {
@@ -25,5 +33,31 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity(response,HttpStatus.NOT_FOUND);
     }
+    //method argumentnot valid exception
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handelMethodArumentNotValidException(MethodArgumentNotValidException ex)
+    {
+
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        Map<String,Object> response=new HashMap<>();
+        allErrors.stream().forEach(objectError -> {
+            String message = objectError.getDefaultMessage();
+            String field = ((FieldError) objectError).getField();
+            response.put(field,message);
+        });
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
