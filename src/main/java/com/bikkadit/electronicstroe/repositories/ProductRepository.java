ab7807ae6ed5d8e4d258c3d9311c7f430f3aa572
umeshstar <umeshahire57@gmail.com>
2023-06-24 15:34:04 +0530

    changes by umesh for custom method in productRepository

--- src/main/java/com/bikkadit/electronicstroe/repositories/ProductRepository.java
+++ src/main/java/com/bikkadit/electronicstroe/repositories/ProductRepository.java
@@ -3,6 +3,11 @@ package com.bikkadit.electronicstroe.repositories;
 import com.bikkadit.electronicstroe.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product , String> {
 
+    // custom finder method for searching
+    List<Product> findByTitleContaining(String subTitle);
+    List<Product>findByLiveTrue();
 }
